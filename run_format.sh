d8431b51048e020c6505ed6981ab0340e1b7da20
MiaoDX <miaodx@hotmail.com>
2020-09-15 20:51:32 +0800

    update format tools

--- run_format.sh
+++ run_format.sh
@@ -2,5 +2,6 @@
 
 clang-format dump_load_speed/include/ocv* -i
 clang-format dump_load_speed/*cpp -i
-clang-format ./data_capture/main.cpp -i
-clang-format ./data_capture/include/* -i
+clang-format thrust_demo/*cu -i
+
+cmake-format */CMakeLists* -i
